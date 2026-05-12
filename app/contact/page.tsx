24b7c6769ddd919885da39e672325b423936ba42
remolalata <lalata.remo@gmail.com>
2026-05-12 21:48:33 +0800

    initialize core pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,3 @@
-export default function HomePage() {
+export default function ContactPage() {
   return <main className="flex flex-1" />;
 }
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,3 +1,3 @@
-export default function HomePage() {
+export default function ServicesPage() {
   return <main className="flex flex-1" />;
 }
